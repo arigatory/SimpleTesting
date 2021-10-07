4413ec1e7dc719a5313b584664f3b982fde3be4e
Ivan MSI <arigatory@gmail.com>
2021-10-07 22:02:50 +0300

    before isolating component

---
+++ SimpleApp.Tests/ProductTests.cs
@@ -0,0 +1,35 @@
+﻿using SimpleApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace SimpleApp.Tests
+{
+    public class ProductTests
+    {
+        [Fact]
+        public void CanChangeProductName()
+        {
+            var p = new Product { Name = "Test", Price = 100M };
+            p.Name = "New Name";
+            Assert.Equal("New Name", p.Name);
+        }
+
+        [Fact]
+        public void CanChangeProductPrice()
+        {
+            // Arrange
+            var p = new Product
+            {
+                Name = "Test",
+                Price = 100M
+            };
+
+            // Act
+            p.Price = 200M; 
+            
+            //Assert
+            Assert.Equal(200M, p.Price); }
+        }
+    }
